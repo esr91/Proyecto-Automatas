13d59c7419dbbbf60cba5362e3422ad0b4954c37
Esteban0589 <eesprimo26@gmail.com>
2014-11-23 16:31:01 -0600

    se agrego metodo printNuestro y se actualizo todo pa solo imprimir con este metodo

--- BreteFinal.py
+++ BreteFinal.py
@@ -42,7 +42,12 @@ t_COMA  = r'\,'
 def t_TAB(t):
 	r'[\t]'
 	#return t
-	#print ('\t', end ="")
+	#printNuestro ('\t', end ="")
+	
+def printNuestro(t):
+	print (cuentaTabs, end = "")
+	print (t)
+
 
 
 
@@ -64,7 +69,7 @@ def t_newline(t):
 
     
 def t_error(t):
-    print("Emoción mal definida '%s'" % t.value[0])
+    printNuestro("Emoción mal definida '%s'" % t.value[0])
     t.lexer.skip(1)
     
 
@@ -76,17 +81,17 @@ lex.lex()
 '''
 def p_emoticonsincejas(p):
     'emoticon : OJOS BOCA'
-    print (p[2])
+    printNuestro (p[2])
 
 def p_emoticonconcejas(p):
     'emoticon : CEJAS OJOS BOCA'
-    print (p[3])
+    printNuestro (p[3])
 '''	
 
 #############MAIN#########
 def p_main(p):
 	'emoticon : instrucciones'
-	print ('\n')
+	printNuestro ('\n')
 #############CUERPO#########
 def p_cuerpo(p):
 	'''cuerpo : instrucciones
@@ -102,20 +107,19 @@ def p_instrucciones(p):
 #############INSTRUCCION#########
 def p_instruccioncuerpo(p):# for, if ,while
 	'''instruccioncuerpo : for acorch cuerpo ccorch'''
-	print (cuentaTabs, end = "")
-	print ("}")
+	printNuestro ("}")
 
 def p_acorch(p):# for, if ,while
 	'acorch : ACORCH'
 	global cuentaTabs
 	cuentaTabs = cuentaTabs+"\t"
-	#print ('*'+cuentaTabs+'*')
+	#printNuestro ('*'+cuentaTabs+'*')
 
 def p_ccorch(p):# for, if ,while
 	'ccorch : CCORCH'
 	global cuentaTabs
 	cuentaTabs = cuentaTabs[:-1]
-	#print ('*'+cuentaTabs+'*')
+	#printNuestro ('*'+cuentaTabs+'*')
 	
 def p_instruccion(p): ### Asignaciones o funciones o metodos
 	'''instruccion : asignacion'''
@@ -124,10 +128,8 @@ def p_instruccion(p): ### Asignaciones o funciones o metodos
 #for (asignaciones;comparaciones;cambios) // Puede ser seguido de una linea donde no requiere {} y cuando es de mas de una linea si requiere {}
 def p_for(p):
 	'for : FOR APARENT	paramFor1	PUNTOCOMA	comparacionesRet	PUNTOCOMA	paramFor3	   CPARENT'
-	print (cuentaTabs, end = "")
-	print (p[3])
-	print (cuentaTabs, end = "")
-	print("Para",p[5],", ",p[7],"{")
+	printNuestro (p[3])
+	printNuestro("Para "+p[5]+", "+p[7]+"{")
 	
 	
 ##########PARAMFOR1######
@@ -160,11 +162,9 @@ def p_asignacion(p):
 	'''asignacion : ID IGUAL valor
 	| RESERVADOS ID IGUAL valor''' 
 	if (len(p) == 4): 
-		print (cuentaTabs, end = "")
-		print (p[1]+"<-"+p[3] )
+		printNuestro (p[1]+"<-"+p[3] )
 	elif (len(p) == 5):  
-		print (cuentaTabs, end = "")
-		print (p[2]+"<-"+p[4] )
+		printNuestro (p[2]+"<-"+p[4] )
 	
 #######COMPARACIONES######## 
 # Recibe el primer parametro del for que puede ser asignaciones o vacia en caso de que las var a usar ya hayan sido declaradas
@@ -228,13 +228,13 @@ def p_valor(p):
 	
 def p_parFor3(p):
 	'paramFor3 : ID'
-	print ("parametro 3 for")
+	printNuestro ("parametro 3 for")
 	
 def p_declaracion(p):
 	'emoticon : RESERVADOS ids PUNTOCOMA'
-	print ("%s declarados:" %p[1])
-	print (p[2])
-	print ("\n")
+	printNuestro ("%s declarados:" %p[1])
+	printNuestro (p[2])
+	printNuestro ("\n")
 	
 #def p_reservados(p):
 #	''' pReservadas : INT
@@ -247,11 +247,11 @@ def p_declaracion(p):
 def p_ids(p):
 	'ids : ID COMA ids'
 	p[0]=p[1]+p[3]
-	'''print ("imprimo desde ids 3")'''
+	'''printNuestro ("imprimo desde ids 3")'''
 		
 def p_ids2(p):
 	'ids : ID'
-	'''print ("imprimo desde ids 1")'''
+	'''printNuestro ("imprimo desde ids 1")'''
 	p[0] = p[1]
 
 
@@ -261,7 +261,7 @@ def p_id(p):
 '''
 def p_prueba1(p):
     'emoticon : NUM'
-    print ("NUM:%s"%p[1])
+    printNuestro ("NUM:%s"%p[1])
 
 def p_prueba2(p):
 	'emoticon : ID'
@@ -271,19 +271,19 @@ def p_empty(p):
 	p[0]=""
 	pass
 	
-def p_tab(p):
-	'''tab : TAB tab
-	| TAB
-	| '''
-	if (len(p) == 3): print ('\t')
-	elif (len(p)== 2) : print ('\t')
+#def p_tab(p):
+#	'''tab : TAB tab
+#	| TAB
+#	| '''
+#	if (len(p) == 3): print ('\t')
+#	elif (len(p)== 2) : printNuestro ('\t')
 	
 	
 def p_error(p):
     if p:
-        print("Error de sintaxis en '%s'" % p.value)
+        printNuestro("Error de sintaxis en '%s'" % p.value)
     else:
-        print(">.> Revise su funcion >.>")
+        printNuestro(">.> Revise su funcion >.>")
 
 import ply.yacc as yacc
 yacc.yacc()
